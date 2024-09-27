e5fc03ea3f0ceae52e808b49dbd925414bbcee94
Sen sathyamony <sensathyamony@gmail.com>
2024-09-27 15:48:50 +0700

    change to sensor camera

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-AngkorVr-0.1";
 const contentToCache = [
     "Build/Web.loader.js",
-    "Build/Web.framework.js",
-    "Build/Web.data",
-    "Build/Web.wasm",
+    "Build/Web.framework.js.unityweb",
+    "Build/Web.data.unityweb",
+    "Build/Web.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
